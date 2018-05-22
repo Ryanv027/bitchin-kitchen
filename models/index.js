55c1dafeed1243c4dcfb2508843c0ca7addc4489
Roberto Perez <roberto@debian.oceannet>
2018-05-22 11:18:39 -0700

    Replaced config.json variables with .env variables

--- models/index.js
+++ models/index.js
@@ -5,14 +5,12 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  dialect: 'postgres'
+});
 
 fs
   .readdirSync(__dirname)
