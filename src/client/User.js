cd2f651f6d9c8c42e8b68415c46c89293a7ef7af
dimstar <kylermagee@gmail.com>
2018-05-28 15:11:31 -0700

    Added search feature to user page. +recipeQuery The recipeQuery gets passed onSubmit. trying to do flat out flitering off key events in the onChange caused massive memory leaks. maybe someone can solve that. Redirect i think is more important.

--- src/client/User.js
+++ src/client/User.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import RecipeScroller from './components/RecipeScroller'
 import { Newnav } from './components/Newnav'
+import { Search } from './components/Search'
 
 export default class User extends React.Component {
+    constructor(props){
+        super(props)
+        
+    }
     render(){
+        console.log( 'User recipeQuery: ', this.props.recipeQuery);
         return (
             <div>
-                <Newnav />
-                <RecipeScroller />
+                <Newnav 
+                    user={this.props.user}
+                    onChange={this.props.onChange}
+                    searchTerm={this.props.searchTerm}
+                    onSubmit={this.props.onSubmit}
+                />
+                <RecipeScroller recipeQuery={this.props.recipeQuery} page={1} />
             </div>
         )
     }
