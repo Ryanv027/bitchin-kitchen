19c200c748da48812f6e3708cbdc6c721e456ecb
dimstar <kylermagee@gmail.com>
2018-05-28 15:41:10 -0700

    Resovled the broken infinity scroll gauntlet. I also removed public folder! We should ditch the public folder as its rebuilt on the fly. Infinity scroll is now fixed. When I was searching I wasn't hitting state properly. we're all good now.

--- src/client/components/RecipeScroller.js
+++ src/client/components/RecipeScroller.js
@@ -10,7 +10,7 @@ export default class RecipeScroller extends React.Component {
         this.state = {
             data: [],    
             page: 1,
-            choice: 'sushi',
+            choice: (this.props.recipeQuery) ? this.props.recipeQuery : 'sushi',
             selectedRecipe: false,
             startingPosition: 780,
             searchTerm: '',
@@ -33,7 +33,13 @@ export default class RecipeScroller extends React.Component {
     }   
     searchRecipes( ){
         console.log( 'yummly query: ', this.props.recipeQuery);
-        let choice = (this.props.recipeQuery) ? this.props.recipeQuery : this.state.choice;
+        let choice;
+        if(this.props.recipeQuery){
+             choice = this.props.recipeQuery;
+             this.state.choice = this.props.recipeQuery;
+        }else{
+            choice = this.state.choice;
+        }
         // another case where you could have made things a litle more DRY
         console.log(choice);
         let url = `/api/recipe-search/${choice}/${this.state.page}`
@@ -74,35 +80,36 @@ export default class RecipeScroller extends React.Component {
         this.setState(({ selectedRecipe: false }))
     }
     handleScroll(){
-        // const position = document.getElementById('scrollboxId').scrollTop
-        // console.log(`position:${position} startingPosition: ${(this.state.startingPosition)}`)
-        // if(position > (this.state.startingPosition)){
-        //     this.setState((prevState) => ({ startingPosition: prevState.startingPosition + (780 * 1.8)}));
-        //     // @ryan this should have been more modular, there should have been a single call to api function that various
-        //     // methods would then interact with. Basically this is all awesome, it just needs to be DRY
-        //     let url = `/api/recipe-search/${this.state.choice}/${this.state.page}`
-        //     let currentRecipes = []
-        //     fetch(url)
-        //         .then(response => {
-        //             return response.json(); 
-        //         }).then(data => {
-        //             // console.log('recipe data: ', data)
-        //             for(let i =0; i < 10; i++){
-        //                 if(data[i].recipeName !== undefined){
-        //                 currentRecipes.push(data[i])
-        //                 }
-        //             }
-        //             // console.log(currentRecipes.length)
-        //             if(currentRecipes.length > 9){
-        //                 this.setState(prevState => ({
-        //                     data: prevState.data.concat(currentRecipes) ,
-        //                     page: prevState.page + 1
-        //                 }))
-        //             }
-        //         }).catch(error => {
-        //             console.log(error)
-        //         }) 
-        // }
+        const position = document.getElementById('scrollboxId').scrollTop
+        console.log(`position:${position} startingPosition: ${(this.state.startingPosition)}`)
+        if(position > (this.state.startingPosition)){
+            this.setState((prevState) => ({ startingPosition: prevState.startingPosition + (780 * 1.8)}));
+            // @ryan this should have been more modular, there should have been a single call to api function that various
+            // methods would then interact with. Basically this is all awesome, it just needs to be DRY
+            let url = `/api/recipe-search/${this.state.choice}/${this.state.page}`
+            console.log(url);
+            let currentRecipes = []
+            fetch(url)
+                .then(response => {
+                    return response.json(); 
+                }).then(data => {
+                    // console.log('recipe data: ', data)
+                    for(let i =0; i < 10; i++){
+                        if(data[i].recipeName !== undefined){
+                        currentRecipes.push(data[i])
+                        }
+                    }
+                    // console.log(currentRecipes.length)
+                    if(currentRecipes.length > 9){
+                        this.setState(prevState => ({
+                            data: prevState.data.concat(currentRecipes) ,
+                            page: prevState.page + 1
+                        }))
+                    }
+                }).catch(error => {
+                    console.log(error)
+                }) 
+        }
     }
     handleSearch(event){
 
@@ -116,7 +123,7 @@ export default class RecipeScroller extends React.Component {
                 <div 
                 className='scrollBox col-9 m-auto text-center'
                 id="scrollboxId"
-                // onScroll={this.handleScroll}
+                onScroll={this.handleScroll}
                 >
                     {this.state.data.length > 0 ? 
                         <Recipe 
