e046f415c5869f750fb1868bacd494cd1be41471
Roberto Perez <roberto@debian.oceannet>
2018-06-01 13:33:05 -0700

    Heroky Deployment Issue

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
         }),
     ] : [
             new webpack.optimize.DedupePlugin(),
-            new webpack.optimize.OccurenceOrderPlugin(),
             new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
         ],
     module: {
